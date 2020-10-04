d9ae4129661dbfd9ece10668940932fb94c684d0
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-10-04 19:43:29 +0200

    Updates

---
+++ precache-manifest.faca5d63a25eb2c7d022cc71d858b559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03db5c8f8dab08bc318fff9c50ac33e6",
+    "url": "/MusicPlayer/index.html"
+  },
+  {
+    "revision": "adfab0e62ea4ec2163c0",
+    "url": "/MusicPlayer/static/css/main.c2bbe833.chunk.css"
+  },
+  {
+    "revision": "cab04af054c77d9e2039",
+    "url": "/MusicPlayer/static/js/2.9dd3598d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MusicPlayer/static/js/2.9dd3598d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adfab0e62ea4ec2163c0",
+    "url": "/MusicPlayer/static/js/main.3c0da86f.chunk.js"
+  },
+  {
+    "revision": "92cec62bc5d46a37e6b7",
+    "url": "/MusicPlayer/static/js/runtime-main.f9f14776.js"
+  },
+  {
+    "revision": "cc4e2784f47909e55d940c014cbef882",
+    "url": "/MusicPlayer/static/media/Spotify_Icon_RGB_White.cc4e2784.png"
+  }
+]);
